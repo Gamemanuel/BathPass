333bb49f2df4faea7a0f26fa77c2cbff033754a8
Gamemanuel <themathnight@gmail.com>
2025-10-01 22:13:43 -0700

    removed the left sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
@@ -20,7 +19,7 @@ export default function Page() {
         } as React.CSSProperties
       }
     >
-      <AppSidebar variant="inset" />
+
       <SidebarInset>
         <SiteHeader />
         <div className="flex flex-1 flex-col">

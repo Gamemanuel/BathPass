d3507aa6464991586aee3372235005554f517f95
Gamemanuel <themathnight@gmail.com>
2025-10-03 18:23:03 -0700

    updated comments in the dashboard page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,21 +8,23 @@ import React from "react"
 export default async function Page() {
     const supabase = await createClient()
 
-    // Check if the User is Logged in
+    // Check if the User is Logged in by calling the supabase cookies
     const {
         data: { user },
     } = await supabase.auth.getUser()
 
-    // If there is no user, then redirect to the login page
+    // If there is no logged-in user, then redirect the user to the login page this prevents unauthorized access.
     if (!user) {
         redirect("/login")
     }
 
     // Fetch data from the Supabase view on the server
     const { data, error } = await supabase
+        // This is the table with the data and the calculated duration time. this is not the table that we send data to fyi.
         .from("bathroom_passes_with_duration")
+        // Select everything in the table
         .select("*")
-        // don't sort by time out from top to bottom
+        // Don't sort by time out from top to bottom
         .order("time_out", { ascending: false })
 
     if (error) {
@@ -31,23 +33,26 @@ export default async function Page() {
         // TODO:(FEATURE) add toast in the top right corner under the user icon with the error message.
     }
 
-    // Pass the fetched data as the initialData prop
+    // Pass the fetched supabase data as the initialData prop so we can feed it to the Data-table component.
     const initialData = data || []
 
     return (
         <SidebarProvider
             style={
+                // Define the Root CSS variables for the project
                 {
+                    // This is the height of the navigation header. this is a static variable and should not change.
                     "--header-height": "calc(var(--spacing) * 16)",
                 } as React.CSSProperties
             }
         >
             <SidebarInset>
+                {/* The Navigation Bar for the website. We pass the --header-height variable into it. */}
                 <SiteHeader />
                 <div className="flex flex-1 flex-col">
                     <div className="@container/main flex flex-1 flex-col gap-2">
                         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                            {/* give the table the data from supabase that we just pulled */}
+                            {/* Pass the Supabase data that we just fetched into the data-table component*/}
                             <DataTable initialData={initialData} />
                         </div>
                     </div>

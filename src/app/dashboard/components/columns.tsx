01983a557b95773da2d6622e8e8718bb5fc14aa6
Gamemanuel <themathnight@gmail.com>
2025-10-03 20:55:09 -0700

    added comments to the columns.tsx file

--- src/app/dashboard/components/columns.tsx
+++ src/app/dashboard/components/columns.tsx
@@ -3,10 +3,12 @@ import {ColumnDef} from "@tanstack/react-table";
 import {BathroomPass} from "@/app/dashboard/components/table-schema";
 
 export const columns: ColumnDef<BathroomPass>[] = [
+    // Checkbox Cell
     {
         id: "select",
         header: ({ table }) => (
             <div className="flex items-center justify-center">
+                {/* Checkbox that selects all the rows for export*/}
                 <Checkbox
                     checked={
                         table.getIsAllPageRowsSelected() ||
@@ -19,6 +21,7 @@ export const columns: ColumnDef<BathroomPass>[] = [
         ),
         cell: ({ row }) => (
             <div className="flex items-center justify-center">
+                {/* Checkbox that adds the row to the list of exports */}
                 <Checkbox
                     checked={row.getIsSelected()}
                     onCheckedChange={(value) => row.toggleSelected(!!value)}
@@ -29,28 +32,37 @@ export const columns: ColumnDef<BathroomPass>[] = [
         enableSorting: false,
         enableHiding: false,
     },
+    // Name Cell
     {
         accessorKey: "name",
-        header: () => <div className="pl-0">Name</div>, // FIX: Remove left padding on the Name column header.
-        cell: ({ row }) => <div className="pl-0">{row.original.name}</div>, // FIX: Remove left padding on the Name column cell.
+        // what is displayed in the header tab
+        header: () => <div>Name</div>,
+        // what is displayed in the rows. this is taken from supabase.
+        cell: ({ row }) => <div>{row.original.name}</div>
     },
+    // Destination Cell. I did nothing fancy to this cell
     {
         accessorKey: "destination",
         header: "Destination",
     },
+    // Time Out Cell that is formated as a time string
     {
         accessorKey: "time_out",
         header: "Time Out",
+        // take the input from supabase and translate it into date format
         cell: ({ row }) => new Date(row.original.time_out).toLocaleTimeString(),
     },
+    // Time In Cell that is formated as a time string
     {
         accessorKey: "time_in",
         header: "Time In",
+        // take the input from supabase and translate it into date format if there is data otherwise it displays "not returned"
         cell: ({ row }) =>
             row.original.time_in
                 ? new Date(row.original.time_in).toLocaleTimeString()
                 : "Not returned",
     },
+    // Total Time cell
     {
         accessorKey: "total_time_spent",
         header: "Total Time",

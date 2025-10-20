17a9783a38941d0cbfd5128ac9cefc9c46b90303
Gamemanuel <themathnight@gmail.com>
2025-10-20 14:19:46 -0700

    added a test for the user menu

--- src/app/test-offline/page.tsx
+++ src/app/test-offline/page.tsx
@@ -1,130 +1,37 @@
-"use client"
-
+import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header"
 import {
-    BadgeCheck,
-    Bell,
-    ChevronsUpDown,
-    CreditCard,
-    LogOut,
-    Sparkles,
-} from "lucide-react"
-
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-} from "@/components/ui/avatar"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-    useSidebar,
+    SidebarInset,
+    SidebarProvider,
 } from "@/components/ui/sidebar"
-import {Button} from "@/components/ui/button"
-import { // These icons where removed, but they need to be added to the user tsx page.
-    Settings,
-    Search,
-    MessageCircleQuestionMark,
-} from "lucide-react"
-import {NavUserClient} from "@/components/nav-user";
-import * as React from "react";
-export default function NavUser(
-//     {
-//                             user,
-//                         }: {
-//     user: {
-//         name: string
-//         email: string
-//         avatar: string
-//     }
-// }
-) {
-    // const { isMobile } = useSidebar()
-    const data = {
-        user: {
-            name: "shadcn",
-            email: "m@example.com",
-            avatar: "/avatars/shadcn.jpg",
-        },
+
+import data from "./data.json"
+
+export default function Page() {
+    const user = {
+        name: "test",
+        email: "test@gmail.com",
+        avatar: "",
     }
     return (
-        <div>
-    {/*<NavUserClient user={data.user} />*/}
-        {/*// <SidebarMenu>*/}
-        {/*//     <SidebarMenuItem>*/}
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        {/*<SidebarMenuButton*/}
-                        <Button
-
-                            size="lg"
-                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-                        >
-                            <Avatar className="h-8 w-8 rounded-lg">
-                                <AvatarImage src="" alt="Gavin" />
-                                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                            </Avatar>
-                            <div className="grid flex-1 text-left text-sm leading-tight">
-                                <span className="truncate font-medium">Gavin</span>
-                                <span className="truncate text-xs">themathnight@gmail.com</span>
-                            </div>
-                            <ChevronsUpDown className="ml-auto size-4" />
-                        </Button>
-                        {/*</SidebarMenuButton>*/}
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent
-                        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-                        // side={isMobile ? "bottom" : "right"}
-                        align="end"
-                        sideOffset={4}
-                    >
-                        <DropdownMenuLabel className="p-0 font-normal">
-                            <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                                <Avatar className="h-8 w-8 rounded-lg">
-                                    <AvatarImage src="" alt="Gavin" />
-                                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                                </Avatar>
-                                <div className="grid flex-1 text-left text-sm leading-tight">
-                                    <span className="truncate font-medium">Gavin</span>
-                                    <span className="truncate text-xs">themathnight@outlook.com</span>
-                                </div>
-                            </div>
-                        </DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuSeparator />
-                        <DropdownMenuGroup>
-                            <DropdownMenuItem>
-                                <BadgeCheck />
-                                Account
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <CreditCard />
-                                Billing
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <Bell />
-                                Notifications
-                            </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                            <LogOut />
-                            Log out
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-        {/*//     </SidebarMenuItem>*/}
-        {/*// </SidebarMenu>*/}
-
-        </div>
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar user={user} variant="inset" />
+            <SidebarInset>
+                <SiteHeader />
+                <div className="flex flex-1 flex-col">
+                    <div className="@container/main flex flex-1 flex-col gap-2">
+                        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                        </div>
+                    </div>
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }

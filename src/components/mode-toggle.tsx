120cfab82a8b96b8a6e13b522f6ed88c5fd2888b
Gamemanuel <themathnight@gmail.com>
2025-10-20 17:01:45 -0700

    removed unneded todo from mode toggle and added a feature idea for v5 it is rated as PRIOTITY-NOT IMPORTANT

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
 import {
     DropdownMenu,
@@ -16,10 +15,10 @@ export function ModeToggle() {
     const { setTheme } = useTheme()
 
     return (
-        // TODO:// make this look like nav-user.tsx
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
+                    {/* TODO:// figure out how to make the system icon appear if the mode is system PRIORITY-NOT IMPORTANT */}
                     <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
                     <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
                     <span className="sr-only">Toggle theme</span>
